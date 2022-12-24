f11797adbb8317172182ca9ef1c079c37d573ff8
blemby12 <agbolamiga@gmail.com>
2022-12-23 16:10:58 -0800

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,9 +7,9 @@
  * @src: second string
  * Return: pointer to designated string
  */
-int main(void)
+
+char *_strcat(char *dest, char *src)
 {
-	char *_strcat(char *dest, char *src)
 	int length, j;
 
 	length = 0;
